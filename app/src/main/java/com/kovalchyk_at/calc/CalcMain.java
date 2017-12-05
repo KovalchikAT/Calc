8192f3d74ee22dba48c5d2bd71c4f463d316b90e
Kovalchyk_AT <Kovalchyk_AT@i.ua>
2017-12-05 15:47:57 +0200

    Initial commit

---
+++ app/src/main/java/com/kovalchyk_at/calc/CalcMain.java
@@ -0,0 +1,13 @@
+package com.kovalchyk_at.calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calc_main);
+    }
+}
